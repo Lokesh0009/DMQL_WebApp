540be931017333ff9bf415ac25b474de4bcfd9a4
Lokesh0009 <101593820+Lokesh0009@users.noreply.github.com>
2024-04-30 05:01:08 -0400

    settings.py

--- dmql_project/settings.py
+++ dmql_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-_wdlt&o))_ndvf7bi)ylrdm=$wx6)#ke7bvf%g$@vf(djoafg9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
